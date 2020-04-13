74753f8bef8b01b10851549016cf1eaebee26f9d
Rafael Menon <rafael13rodrigo@gmail.com>
2020-04-13 16:51:13 -0300

    Upload e streaming das aulas no arquivo de rotas.

--- config/routes.js
+++ config/routes.js
@@ -1,4 +1,8 @@
-module.exports = app => {
+const multer = require("multer");
+const fs = require("fs");
+const path = require("path");
+
+module.exports = (app) => {
   app.post("/sessions", app.api.auth.signin);
   app.post("/validateToken", app.api.auth.validateToken);
 
@@ -56,4 +60,62 @@ module.exports = app => {
     .route("/modulos/:idCurso")
     .all(app.config.passport.authenticate())
     .get(app.api.modulos.get);
+
+  //Aqui recebe o upload do video, será separado posteriormente.
+  const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "public");
+    },
+    filename: function (req, file, cb) {
+      cb(null, Date.now() + "-" + file.originalname);
+    },
+  });
+
+  const upload = multer({ storage: storage }).single("file");
+
+  app.post("/upload", function (req, res) {
+    upload(req, res, function (err) {
+      if (err instanceof multer.MulterError) {
+        return res.status(500).json(err);
+      } else if (err) {
+        return res.status(500).json(err);
+      }
+
+      return res.status(200).send(req.file);
+    });
+  });
+
+  //Aqui faz o streaming do video, será seperado posteriormente.
+  app.get("/aula/:nomeAula", function (req, res) {
+    console.log("params ->>>>><<<<<<", req.params.nomeAula);
+    const path = `public/${req.params.nomeAula}`;
+    const stat = fs.statSync(path);
+    const fileSize = stat.size;
+    const range = req.headers.range;
+
+    if (range) {
+      const parts = range.replace(/bytes=/, "").split("-");
+      const start = parseInt(parts[0], 10);
+      const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
+
+      const chunksize = end - start + 1;
+      const file = fs.createReadStream(path, { start, end });
+      const head = {
+        "Content-Range": `bytes ${start}-${end}/${fileSize}`,
+        "Accept-Ranges": "bytes",
+        "Content-Length": chunksize,
+        "Content-Type": "video/mp4",
+      };
+
+      res.writeHead(206, head);
+      file.pipe(res);
+    } else {
+      const head = {
+        "Content-Length": fileSize,
+        "Content-Type": "video/mp4",
+      };
+      res.writeHead(200, head);
+      fs.createReadStream(path).pipe(res);
+    }
+  });
 };
